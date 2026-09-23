4d4810c3b09bca6f4109850f4f30dbe954e363eb
Arvid Rolander <arvid.rolander@gmail.com>
2026-09-23 12:12:54 +0200

    add: channel bias, im2col, reshape, conv2d_im2col, maxpool2d

--- include/ops.hpp
+++ include/ops.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "graph.hpp"
 #include "tensor.hpp"
+#include <vector>
 
 namespace deeplib {
 
@@ -23,4 +24,19 @@ Tensor *add_bias(Graph *g, Tensor *x, Tensor *b);
 Tensor *flatten(Graph *g, Tensor *x);
 
 Tensor *conv2d(Graph *g, Tensor *x, Tensor *k, int stride = 1, int padding = 0);
+
+Tensor *add_channel_bias(Graph *g, Tensor *x, Tensor *b); // x (N,C,H,W), b (C)
+
+Tensor *im2col(Graph *g, Tensor *x, int Kh, int Kw, int stride = 1,
+               int padding = 0);
+
+Tensor *reshape_op(Graph *g, Tensor *x, const std::vector<int> &new_shape);
+
+Tensor *permute_nhwc_to_nchw(Graph *g, Tensor *x);
+
+Tensor *conv2d_im2col(Graph *g, Tensor *x, Tensor *k, int stride = 1,
+                      int padding = 0);
+
+Tensor *maxpool2d(Graph *g, Tensor *x, int Kh, int Kw, int stride);
+
 } // namespace deeplib
--- src/ops.cpp
+++ src/ops.cpp
@@ -1,9 +1,12 @@
 #include "ops.hpp"
 #include "graph.hpp"
 #include "tensor.hpp"
+#include "utils.hpp"
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
 #include <iterator>
+#include <limits>
 #include <vector>
 
 namespace deeplib {
@@ -215,25 +218,8 @@ Tensor *add_bias(Graph *g, Tensor *x, Tensor *b) {
 }
 
 Tensor *flatten(Graph *g, Tensor *x) {
-  int bs = x->shape[0];
-  int flat_size = x->size() / bs;
-  int batch_stride = x->strides[0];
-  Tensor *out = g->make({bs, flat_size}, x->requires_grad);
-  for (int b = 0; b < bs; b++) {
-    for (int i = 0; i < flat_size; i++) {
-      out->at(b, i) = x->data[b * batch_stride + i];
-    }
-  }
-  out->parents = {x};
-  out->backward_fn = [x, out, bs, flat_size, batch_stride]() {
-    for (int b = 0; b < bs; b++) {
-      for (int i = 0; i < flat_size; i++) {
-        x->grad[b * batch_stride + i] += out->grad_at(b, i);
-      }
-    }
-  };
 
-  return out;
+  return reshape_op(g, x, {x->shape[0], x->size() / x->shape[0]});
 }
 
 Tensor *conv2d(Graph *g, Tensor *x, Tensor *k, int stride, int padding) {
@@ -316,4 +302,216 @@ Tensor *conv2d(Graph *g, Tensor *x, Tensor *k, int stride, int padding) {
   return out;
 }
 
+Tensor *add_channel_bias(Graph *g, Tensor *x, Tensor *b) {
+  assert(x->shape[1] == b->shape[0]);
+  Tensor *out = g->make(x->shape);
+
+  int bs = x->shape[0];
+  int channels = x->shape[1];
+  int H = x->shape[2];
+  int W = x->shape[3];
+
+  // Forward pass
+  for (int n = 0; n < bs; n++) {
+    for (int c = 0; c < channels; c++) {
+      for (int i = 0; i < H; i++) {
+        float bc = b->data[c];
+        for (int j = 0; j < W; j++) {
+          out->at(n, c, i, j) = x->at(n, c, i, j) + bc;
+        }
+      }
+    }
+  }
+
+  out->parents = {x, b};
+  out->backward_fn = [x, b, out, bs, channels, H, W]() {
+    for (int n = 0; n < bs; n++) {
+      for (int c = 0; c < channels; c++) {
+        float b_grad = 0.0f;
+        for (int i = 0; i < H; i++) {
+          for (int j = 0; j < W; j++) {
+            b_grad += out->grad_at(n, c, i, j);
+            x->grad_at(n, c, i, j) += out->grad_at(n, c, i, j);
+          }
+        }
+        b->grad[c] += b_grad;
+      }
+    }
+  };
+  return out;
+}
+
+Tensor *im2col(Graph *g, Tensor *x, int Kh, int Kw, int stride, int padding) {
+  assert(x->shape.size() == 4);
+
+  assert(stride >= 1 && padding >= 0);
+
+  int bs = x->shape[0]; // batch size
+  int H = x->shape[2];
+  int W = x->shape[3];
+  assert(H + 2 * padding >= Kh && W + 2 * padding >= Kw);
+  int Oh = (H + 2 * padding - Kh) / stride + 1;
+  int Ow = (W + 2 * padding - Kw) / stride + 1;
+  int C_in = x->shape[1];
+
+  Tensor *out = g->make({bs * Oh * Ow, C_in * Kh * Kw}, x->requires_grad);
+  for (int n = 0; n < bs; n++) {
+    for (int i = 0; i < Oh; i++) {
+      for (int j = 0; j < Ow; j++) {
+        int r = n * Oh * Ow + i * Ow + j;
+        for (int c = 0; c < C_in; c++) {
+          for (int u = 0; u < Kh; u++) {
+            int hi = stride * i + u - padding;
+            for (int v = 0; v < Kw; v++) {
+              int wi = stride * j + v - padding;
+              int q = c * Kh * Kw + u * Kw + v;
+              out->at(r, q) = (hi < 0 || hi >= H || wi < 0 || wi >= W)
+                                  ? 0.0f
+                                  : x->at(n, c, hi, wi);
+            }
+          }
+        }
+      }
+    }
+  }
+  out->parents = {x};
+  out->backward_fn = [x, out, Kh, Kw, stride, padding, bs, Oh, Ow, C_in, H,
+                      W]() {
+    for (int n = 0; n < bs; n++) {
+      for (int i = 0; i < Oh; i++) {
+        for (int j = 0; j < Ow; j++) {
+          int r = n * Oh * Ow + i * Ow + j;
+          for (int c = 0; c < C_in; c++) {
+            for (int u = 0; u < Kh; u++) {
+              int hi = stride * i + u - padding;
+              for (int v = 0; v < Kw; v++) {
+                int wi = stride * j + v - padding;
+                int q = c * Kh * Kw + u * Kw + v;
+                if (hi < 0 || hi >= H || wi < 0 || wi >= W)
+                  continue;
+                x->grad_at(n, c, hi, wi) += out->grad_at(r, q);
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+  return out;
+}
+
+Tensor *reshape_op(Graph *g, Tensor *x, const std::vector<int> &new_shape) {
+  assert(x->size() == product(new_shape));
+  Tensor *out = g->make(new_shape, x->requires_grad);
+
+  // forward pass
+  std::copy(x->data.begin(), x->data.end(), out->data.begin());
+
+  // backward pass
+  out->parents = {x};
+  out->backward_fn = [x, out]() {
+    const int n = x->size();
+    for (int i = 0; i < n; i++) {
+      x->grad[i] += out->grad[i];
+    }
+  };
+  return out;
+}
+
+Tensor *permute_nhwc_to_nchw(Graph *g, Tensor *x) {
+  assert(x->shape.size() == 4);
+
+  int N = x->shape[0], H = x->shape[1], W = x->shape[2], C = x->shape[3];
+
+  Tensor *out = g->make({N, C, H, W}, x->requires_grad);
+
+  for (int n = 0; n < N; n++) {
+    for (int h = 0; h < H; h++) {
+      for (int w = 0; w < W; w++) {
+        for (int c = 0; c < C; c++) {
+          out->at(n, c, h, w) = x->at(n, h, w, c);
+        }
+      }
+    }
+  }
+  out->parents = {x};
+  out->backward_fn = [x, out, N, C, H, W]() {
+    for (int n = 0; n < N; n++) {
+      for (int h = 0; h < H; h++) {
+        for (int w = 0; w < W; w++) {
+          for (int c = 0; c < C; c++) {
+            x->grad_at(n, h, w, c) += out->grad_at(n, c, h, w);
+          }
+        }
+      }
+    }
+  };
+  return out;
+}
+
+Tensor *conv2d_im2col(Graph *g, Tensor *x, Tensor *k, int stride, int padding) {
+  assert(x->shape.size() == 4);
+  assert(k->shape.size() == 4);
+  assert(x->shape[1] == k->shape[1]);
+  assert(stride >= 1 && padding >= 0);
+
+  int N = x->shape[0], C = x->shape[1], H = x->shape[2], W = x->shape[3];
+  int F = k->shape[0], Kh = k->shape[2], Kw = k->shape[3];
+  assert(H + 2 * padding >= Kh && W + 2 * padding >= Kw);
+
+  int Oh = (H + 2 * padding - Kh) / stride + 1;
+  int Ow = (W + 2 * padding - Kw) / stride + 1;
+
+  Tensor *cols = im2col(g, x, Kh, Kw, stride, padding); // (N*Oh*Ow, C*Kh*Kw)
+  Tensor *kf = reshape_op(g, k, {F, C * Kh * Kw});
+  Tensor *kt = transpose(g, kf);   // (C*Kh*Kw, F)
+  Tensor *r = matmul(g, cols, kt); // (N*Oh*Ow, F)
+  Tensor *rr = reshape_op(g, r, {N, Oh, Ow, F});
+  return permute_nhwc_to_nchw(g, rr);
+}
+
+Tensor *maxpool2d(Graph *g, Tensor *x, int Kh, int Kw, int stride) {
+  assert(stride >= 1);
+
+  int N = x->shape[0], C = x->shape[1], H = x->shape[2], W = x->shape[3];
+  assert(H >= Kh && W >= Kw);
+  int Oh = (H - Kh) / stride + 1;
+  int Ow = (W - Kw) / stride + 1;
+
+  Tensor *out = g->make({N, C, Oh, Ow}, x->requires_grad);
+  std::vector<int> argmax(out->size());
+  for (int n = 0; n < N; n++) {
+    for (int c = 0; c < C; c++) {
+      for (int i = 0; i < Oh; i++) {
+        for (int j = 0; j < Ow; j++) {
+          float running_max = -std::numeric_limits<float>::infinity();
+          int amax; // local argmax
+          for (int u = 0; u < Kh; u++) {
+            int hi = stride * i + u;
+            for (int v = 0; v < Kw; v++) {
+              int wi = stride * j + v;
+              float p = x->at(n, c, hi, wi);
+              if (p > running_max) {
+                running_max = p;
+                amax = n * x->strides[0] + c * x->strides[1] +
+                       hi * x->strides[2] + wi * x->strides[3];
+                int out_idx = n * out->strides[0] + c * out->strides[1] +
+                              i * out->strides[2] + j * out->strides[3];
+                out->data[out_idx] = running_max;
+                argmax[out_idx] = amax;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  out->parents = {x};
+  out->backward_fn = [x, out, argmax]() {
+    for (size_t idx = 0; idx < argmax.size(); idx++) {
+      x->grad[argmax[idx]] += out->grad[idx];
+    }
+  };
+  return out;
+}
 } // namespace deeplib
